73129f3cf8f491c9c323efc521d8bf515087c5ac
ybnr1130 <muuckk@gmail.com>
2022-01-03 10:23:17 +0900

    First commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by 薮内紀仁 on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
