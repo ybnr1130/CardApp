0738fccf3f99fd30147e8d154bed7d287fd301fa
ybnr1130 <muuckk@gmail.com>
2022-01-04 07:37:45 +0900

    Add random card

--- CardApp/ContentView.swift
+++ CardApp/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    var cards = ["かばん" : "bag",
+                 "自動車" : "car",
+                 "誕生日" : "birthday"]
+    @State var japanese = "かばん"
+    @State var isJapanese = true
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(isJapanese ? japanese : cards[japanese]!)
+            HStack {
+                Button(/*@START_MENU_TOKEN@*/"裏返す"/*@END_MENU_TOKEN@*/) {
+                    self.isJapanese.toggle()
+                }
+                Button(/*@START_MENU_TOKEN@*/"次へ"/*@END_MENU_TOKEN@*/) {
+                    self.isJapanese = true
+                    self.japanese = self.cards.randomElement()!.key
+                }
+            }
+        }
     }
 }
 
