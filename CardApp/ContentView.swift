5e4780c312646c23f8a5966ddbc88608155f3696
ybnr1130 <muuckk@gmail.com>
2022-01-07 13:30:57 +0900

    Add color

--- CardApp/ContentView.swift
+++ CardApp/ContentView.swift
@@ -15,26 +15,31 @@ struct ContentView: View {
     @State var isJapanese = true
     var body: some View {
         ZStack {
-            Image("card")
-                .resizable()
-                .frame(width: 300.0, height: 133)
-                .shadow(radius: 3)
-                .rotation3DEffect(.degrees(isJapanese ? 0 : 180),
-                                axis: (x: 0, y: 1, z: 0))
-                .animation(.spring())
-                .onTapGesture {
-                    self.isJapanese.toggle()
-                }
-        }
-        VStack {
-            Text(isJapanese ? japanese : cards[japanese]!)
-            HStack {
-                Button(/*@START_MENU_TOKEN@*/"裏返す"/*@END_MENU_TOKEN@*/) {
-                    self.isJapanese.toggle()
-                }
-                Button(/*@START_MENU_TOKEN@*/"次へ"/*@END_MENU_TOKEN@*/) {
-                    self.isJapanese = true
-                    self.japanese = self.cards.randomElement()!.key
+            Color(red: 0.97, green: 0.96, blue: 0.56)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    Image("card")
+                    .resizable()
+                    .frame(width: 300.0, height: 133)
+                    .shadow(radius: 3)
+                    .rotation3DEffect(.degrees(isJapanese ? 0 : 180),
+                                    axis: (x: 0, y: 1, z: 0))
+                    .animation(.spring())
+                    .onTapGesture {
+                        self.isJapanese.toggle()
+                    }
+                    Text(isJapanese ? japanese : cards[japanese]!)
+                        .font(.largeTitle)
+                }.padding()
+                HStack {
+                    Button(/*@START_MENU_TOKEN@*/"裏返す"/*@END_MENU_TOKEN@*/) {
+                            self.isJapanese.toggle()
+                    }
+                    Button(/*@START_MENU_TOKEN@*/"次へ"/*@END_MENU_TOKEN@*/) {
+                        self.isJapanese = true
+                        self.japanese = self.cards.randomElement()!.key
+                    }
                 }
             }
         }
